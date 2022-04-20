00814bb7b24b7abcee42dbb79688fed9786f02d8
Jack W. Tussing <58670450+tussingj89@users.noreply.github.com>
2022-04-20 00:03:40 -0400

    Update app.js
    update #1 : created a deck class to represent my deck of cards, finished my shuffle function to randomly mix the array of values and then divide them into the player array and the computer array

--- WAR/app.js
+++ WAR/app.js
@@ -5,23 +5,32 @@ const compCards = document.getElementById('compCards')
 const compPoints = document.getElementById('compPoints')
 const computer = document.getElementById('computer')
 const results = document.getElementById('result')
-const deal = getElementById('deal')
+// const deal = getElementById('deal')
 
 let playerArray = []
 let compArray = []
 let pPoints = 0
 let cPoints = 0
-// make deck a object with a sub class of suit and a sub class of 1-13 ask andrew if this will work
-class Suit{
-    constructor(image){
-    this.Image = image
-    this.cards = [2,3,4,5,6,7,8,9,10,11,12,13,14]
+let deck;
+
+class Deck {
+    constructor() {
+      this.deck = [];
+  
+      const suits = ['Hearts', 'Spades', 'Clubs', 'Diamonds'];
+      const values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace'];
+  
+      for (let suit in suits) {
+        for (let value in values) {
+          this.deck.push(`${values[value]}, ${suits[suit]}`);
+        }
+      }
     }
-}
-const hearts = new Suit('./images/heart.png')
-const diamond = new Suit('./images/diamond.png')
-const clubs = new Suit('./images/clubs.png')
-const spade = new Suit('./images/spade.png')
+  }
+  
+  const deck1 = new Deck();
+  deck = deck1;
+//   console.log(deck1.deck);
 
 deal.addEventListener('click', war())
 
@@ -32,13 +41,29 @@ function check(){
     // checks which players card was higher
 }
 function shuffle(){
-    // on page load random devides 52 "cards" into playerArray and compArray
+    deck.deck
+    let set = 1;
+    // console.log(deck.deck)
+    let shuffled = deck.deck.sort((a,b) => 0.5 - Math.random())
+    // console.log(shuffled)
+    for(let i = 0; i < shuffled.length ; i++) {
+        let random = Math.floor(Math.random() * shuffled.length);
+        if(set === 1) {
+            playerArray.push(shuffled[i]);
+            set++
+        } else {
+            compArray.push(shuffled[i]);
+            set--
+        }
 
+
+    }
+    return [playerArray, compArray];
+   
 }
+console.log(playerArray)
+console.log(compArray)
 function play(){
     shuffle()
 }
 play()
-
-
-
