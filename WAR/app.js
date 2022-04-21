81ba7c4323628e909cdbb0f03f396a547b23a2c9
Jack W. Tussing <58670450+tussingj89@users.noreply.github.com>
2022-04-20 23:55:19 -0400

    Update app.js
    update #2: changes elements of the class, started deal function

--- WAR/app.js
+++ WAR/app.js
@@ -1,42 +1,51 @@
-const playCards = document.getElementById('playerCards')
-const playerPoints = document.getElementById('playerPoints')
-const player = document.getElementById('player')
-const compCards = document.getElementById('compCards')
-const compPoints = document.getElementById('compPoints')
-const computer = document.getElementById('computer')
-const results = document.getElementById('result')
-// const deal = getElementById('deal')
+const playCards = document.getElementById('playerCards');
+const playerPoints = document.getElementById('playerPoints');
+const player = document.getElementById('player');
+const compCards = document.getElementById('compCards');
+const compPoints = document.getElementById('compPoints');
+const computer = document.getElementById('computer');
+const results = document.getElementById('result');
+const deal = document.getElementById('deal');
 
-let playerArray = []
-let compArray = []
-let pPoints = 0
-let cPoints = 0
+let playerArray = [];
+let compArray = [];
+let pPoints = 0;
+let cPoints = 0;
 let deck;
 
 class Deck {
     constructor() {
-      this.deck = [];
+        this.deck = [];
   
-      const suits = ['Hearts', 'Spades', 'Clubs', 'Diamonds'];
-      const values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace'];
+        const suits = ['Hearts', 'Spades', 'Clubs', 'Diamonds'];
+        const names = [[2,2], [3,3], [4,4], [5,5], [6,6], [7,7], [8,8], [9,9], [10,10], ['Jack',11], ['Queen',12], ['King',13], ['Ace',14]];
   
-      for (let suit in suits) {
-        for (let value in values) {
-          this.deck.push(`${values[value]}, ${suits[suit]}`);
+    for (let suit in suits) {
+        for (let name in names) {
+            this.deck.push(`${names[name]}, ${suits[suit]}`);
         }
-      }
     }
-  }
+    }
+}
   
   const deck1 = new Deck();
   deck = deck1;
 //   console.log(deck1.deck);
 
-deal.addEventListener('click', war())
+deal.addEventListener('click', function(){
+    let tempCard1 = playerArray[0];
+    let tempCard2 = compArray[0];
+    tempCard1.split(',');
+    tempCard2.split(',');
+     console.log(tempCard1 + ' p1  ' + playerIndex);
+     console.log(tempCard2 + '  c1  ' + compIndex);
+    //  split string at ,
+    //  display them inside cards
+    // set background image to suit
+    // trigger check function
 
-function war(){
-    // deal card to player and computer
-}
+})
+
 function check(){
     // checks which players card was higher
 }
@@ -61,8 +70,8 @@ function shuffle(){
     return [playerArray, compArray];
    
 }
-console.log(playerArray)
-console.log(compArray)
+// console.log(playerArray)
+// console.log(compArray)
 function play(){
     shuffle()
 }
