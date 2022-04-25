cb5beec536f6dc5ea76153f8e29e8c1fc566fad0
Jack W. Tussing <58670450+tussingj89@users.noreply.github.com>
2022-04-25 00:03:15 -0400

    Update app.js
    update #8: added check function logic, issue still remains with if else statements need fixed

--- WAR/app.js
+++ WAR/app.js
@@ -16,7 +16,7 @@ let deck;
 class Deck {
     constructor() {
         this.deck = [];
-  
+
         const suits = ['Hearts', 'Spades', 'Clubs', 'Diamonds'];
         const names = [[2,2], [3,3], [4,4], [5,5], [6,6], [7,7], [8,8], [9,9], [10,10], ['Jack',11], ['Queen',12], ['King',13], ['Ace',14]];
   
@@ -27,34 +27,65 @@ class Deck {
     }
     }
 }
-  
   const deck1 = new Deck();
   deck = deck1;
-//   console.log(deck1.deck);
 
-deal.addEventListener('click', function(){
+  deal.addEventListener('click', function(){
     let tempCard1 = playerArray[0].split(',');
     let tempCard2 = compArray[0].split(',');
-     console.log(tempCard1);
-     console.log(tempCard2);
      player.setAttribute('class', tempCard1[2])
      computer.setAttribute('class', tempCard2[2])
      player.innerHTML = tempCard1[0]
      computer.innerHTML = tempCard2[0]
-    //  display them inside cards
-    // trigger check function
+     check(tempCard1, tempCard2)
+    if(playerArray === 0 || compArray === 0){
+        console.log('Game Over')
+        results.innerHTML = 'Game Over'
+        deal.removeEventListener('click')
+    }
 
 })
 
-function check(){
-    // checks which players card was higher
+function check(tempCard1, tempCard2){
+    console.log(tempCard1, tempCard2)
+
+    if(tempCard1[1] > tempCard2[1]){
+        console.log('you win')
+        results.innerHTML = 'You Win!'
+        pPoints++
+        playerPoints.innerHTML = pPoints
+        playerArray.shift(1)
+        compArray.shift(1)
+        playerArray.push(playerArray[0], compArray[0])
+        playCards.innerHTML = playerArray.length
+        compCards.innerHTML = compArray.length
+  
+
+    }
+    else if(tempCard1[1] === tempCard2[1]){
+        console.log('tie')
+        results.innerHTML = 'Its a Tie'
+        playerArray.shift(1)
+        playerArray.push(playerArray[0])
+        compArray.shift(1)
+        compArray.push(compArray[0])
+    }
+    else{
+        console.log('you lose')
+        results.innerHTML = 'You Lose'
+        cPoints++
+        compPoints.innerHTML = cPoints
+        playerArray.shift(1)
+        compArray.shift(1)
+        compArray.push(playerArray[0], compArray[0])
+        playCards.innerHTML = playerArray.length
+        compCards.innerHTML = compArray.length
+    }
 }
 function shuffle(){
     deck.deck
     let set = 1;
-    // console.log(deck.deck)
     let shuffled = deck.deck.sort((a,b) => 0.5 - Math.random())
-    // console.log(shuffled)
     for(let i = 0; i < shuffled.length ; i++) {
         let random = Math.floor(Math.random() * shuffled.length);
         if(set === 1) {
@@ -64,15 +95,16 @@ function shuffle(){
             compArray.push(shuffled[i]);
             set--
         }
-
-
     }
     return [playerArray, compArray];
    
 }
-// console.log(playerArray)
-// console.log(compArray)
 function play(){
     shuffle()
+    playCards.innerHTML = playerArray.length
+    compCards.innerHTML = compArray.length
+    playerPoints.innerHTML = pPoints
+    compPoints.innerHTML = cPoints
 }
+
 play()
